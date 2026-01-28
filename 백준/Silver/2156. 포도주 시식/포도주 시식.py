6a0e59119ed69250fc5256d47e6a369136bfce5a
tcrnela <tcrnela@gmail.com>
2026-01-28 18:52:46 +0900

    [Silver I] Title: 포도주 시식, Time: 260 ms, Memory: 111060 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = [0] * (n+2)
+dp = [0] * (n+2)
+
+for i in range (1, n+1):
+    a[i] = int(input())
+
+dp[1] = a[1]
+dp[2] = a[2] + a[1]
+
+for i in range (3, n+1):
+    for j in range (3, i+1):
+        dp[i] = max(dp[i], dp[i-j+1] + a[i], dp[i-j] + a[i-1] + a[i])
+
+print(max(dp))
